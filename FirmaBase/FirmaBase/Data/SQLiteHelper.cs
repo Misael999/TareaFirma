98a59cfbf86f981cb1ef9fd861022a705a1a56c9
Misael Ordonez <alejandro.corrales999@gmail.com>
2021-11-02 17:01:37 -0600

    Tarea en Grupo

---
+++ FirmaBase/FirmaBase/Data/SQLiteHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using FirmaBase.Models;
+using System.Threading.Tasks;
+
+namespace FirmaBase.Data
+{
+   public class SQLiteHelper
+    {
+        SQLiteAsyncConnection db;
+        public SQLiteHelper(string dbPath)
+        {
+            db = new SQLiteAsyncConnection(dbPath);
+            db.CreateTableAsync<Persona>().Wait();
+        }
+
+        public Task<int> SavePersona(Persona person)
+        {
+            if (person.Idpersona != 0)
+            {
+                return db.UpdateAsync(person);
+                ;
+            }
+            else
+            {
+                return db.InsertAsync(person);
+            }
+        }
+        /// <summary>
+        /// Recuperar todos los personas
+        /// </summary>
+        /// <returns></returns>
+        public Task<List<Persona>> GetPersonasAync()
+        {
+            return db.Table<Persona>().ToListAsync();
+        }
+        /// <summary>
+        /// Recupera las personas por la identidad
+        /// </summary>
+        /// <param name="identidad">Identidad de la persona requerida</param>
+        /// <returns></returns>
+        public Task<Persona> GetPersonaByIdAsync(String nomb)
+        {
+            return db.Table<Persona>().Where(a => a.nombre == nomb).FirstOrDefaultAsync();
+        }
+
+        public Task<int> DropPersonaAsync(Persona person)
+        {
+            return db.DeleteAsync(person);
+        }
+
+    }
+}
